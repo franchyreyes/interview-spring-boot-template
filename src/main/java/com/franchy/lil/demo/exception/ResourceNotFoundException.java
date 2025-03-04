d8e8a55557838ef26308dbae83f7134f221aa1b6
Franchy Reyes <freyesr@copaair.com>
2025-03-04 13:04:48 -0400

    add a custom response API and exceptions

---
+++ src/main/java/com/franchy/lil/demo/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.franchy.lil.demo.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
